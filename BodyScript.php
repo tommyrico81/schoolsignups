ffe13cda840c9be33e35e5808adb86872249ef1c
tommyrico81 <tommyrico81@gmail.com>
2017-08-15 16:57:07 +0000

    v1.0

---
+++ BodyScript.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '<body>';
+echo '<input type="button">Add Child</button><br>';
+echo '<input type="button">View Entries</button><br>';
+echo '<input type="button">Notifications</button><br>';
+echo '<input type="button">Lottery Selection</button><br>';
+echo '</body>';
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 include '/opt/app-root/src/HeaderScript.php';
 ?>
-<body>
-
-This is a test.....
-
-</body>
+<?php
+include '/opt/app-root/src/BodyScript.php';
+?>
 <?php
 include '/opt/app-root/src/FooterScript.php';
 ?>
