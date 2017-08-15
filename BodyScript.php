838ba16d33d1815b9ea71d4d2022cf9b0524e277
tommyrico81 <tommyrico81@gmail.com>
2017-08-15 17:24:53 +0000

    v1.0

--- BodyScript.php
+++ BodyScript.php
@@ -1,10 +1,10 @@
 <?php
 
 echo '<body>';
-echo '<input type="button">Add Child</button><br>';
-echo '<input type="button">View Entries</button><br>';
-echo '<input type="button">Notifications</button><br>';
-echo '<input type="button">Lottery Selection</button><br>';
+echo '<button type="button">Add Child</button><br>';
+echo '<button type="button">View Entries</button><br>';
+echo '<button type="button">Notifications</button><br>';
+echo '<button type="button">Lottery Selection</button><br>';
 echo '</body>';
 
 ?>
