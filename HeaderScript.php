45eefe2b45311252c6b10201e508412ff6cb22aa
tommyrico81 <tommyrico81@gmail.com>
2017-08-18 00:27:06 +0000

    v1.0

--- HeaderScript.php
+++ HeaderScript.php
@@ -2,12 +2,8 @@
     echo '
             <html>
             <head>
-            <link rel="stylesheet" href="css/themes/Theme_V1.min.css" />
-            <link rel="stylesheet" href="css/themes/jquery.mobile.icons.min.css" />
-            <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
-            <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-            <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
             </head>
             <title>SchoolSignUps
-            </title>';
+            </title>
+            ';
 ?>
